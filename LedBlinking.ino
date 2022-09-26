ba44a857c64ac931fdd3f2332c60eaef54437f45
Syed Murtaza Ali Naqvi <86062433+Murtazaaly01@users.noreply.github.com>
2022-09-26 14:18:17 +0500

    - notes

---
+++ LedBlinking.ino
@@ -0,0 +1,16 @@
+int d = 500;//code to create a  wave of blinking LEDs with half second delay.
+void setup()
+{
+pinMode(2, OUTPUT); // LED 1 control pin is set up as an output
+pinMode(3, OUTPUT); // same for LED 2 to LED 5
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);}
+// using for loop
+for ( int a = 5 ; a > 1 ; a-- )
+{
+digitalWrite(a, HIGH);
+delay(d);
+digitalWrite(a, LOW);
+delay(d);
+}
